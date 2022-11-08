92df805c28076977517599a570963fd20050f7dc
Lucas Peterson <notlucaspeterson@gmail.com>
2022-11-08 15:49:41 +0300

    #Deleted

--- markups.py
+++
@@ -1,6 +0,0 @@
-from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
-
-btnStart = KeyboardButton('Начать!')
-
-mainMenu = ReplyKeyboardMarkup(resize_keyboard = True)
-mainMenu.add(btnStart)
